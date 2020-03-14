697b94198480448cbfa2da68a0989f46897d3a23
Hung Tran <hung.tran@wizeline.com>
2020-03-14 21:05:07 +0700

    Add AKS cluster

---
+++ aks.tf
@@ -0,0 +1,7 @@
+module "aks" {
+  source              = "Azure/aks/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+#   client_id           = "your-service-principal-client-appid"
+#   client_secret       = "your-service-principal-client-password"
+  prefix              = "prefix"
+}
