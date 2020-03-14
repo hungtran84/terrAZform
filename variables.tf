2abe3d92fd7df7d02329b0b16283c5fa0957fbeb
Hung Tran <hung.tran@wizeline.com>
2020-03-14 21:20:03 +0700

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "client_id" {
+  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
+}
+
+variable "client_secret" {
+  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
+}
