b968357ab46543f4aaf21329ece16c2b6cd490cc
Hung Tran <hung.tran@wizeline.com>
2020-03-27 23:09:29 +0700

    Deploy VM scale set

--- vnet.tf
+++ vnet.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   }
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
