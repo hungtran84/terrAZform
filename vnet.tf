085ec27d8ba22fb0c0110a6048a18f4e9d2a7c0b
Hung Tran <hung.tran@wizeline.com>
2020-03-28 13:47:32 +0700

    Add Azure bastion

--- vnet.tf
+++ vnet.tf
@@ -15,3 +15,18 @@ resource "azurerm_subnet" "internal" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.1.0/24"
 }
+
+resource "azurerm_subnet" "bastionSubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.1.224/27"
+}
+
+resource "azurerm_public_ip" "bastionIP" {
+  name                = "publicip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
